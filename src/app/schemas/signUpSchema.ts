331a50cb66242a81f3c43183c2eb9e469b178eb3
Ayush Saha <ayushsaha9928@gamil.com>
2024-07-01 12:53:53 +0530

    All new changes

---
+++ src/app/schemas/signUpSchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(2, "Username must be atleast 2 characters")
+  .max(20, "Username must be no more then 20 characters")
+  .regex(
+    /^[a-zA-Z][a-zA-Z0-9]{2,15}$/,
+    "Username must not contain sepcial character"
+  );
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be at least 6 characters" }),
+});
